6534dd1389f2f4bd2c7c7ae57448c9ecd94f78f6
lorenzomaiocchi <lorenzo.maiocchi06@gmail.com>
2025-06-23 17:37:34 +0200

    Recoding in process

--- Scripts/_00_Data_Cleaning.R
+++ Scripts/_00_Data_Cleaning.R
@@ -32,6 +32,7 @@ load("C:/Users/Merio/Desktop/Uni/Multivariate/FINAL EXAM/Paper-data/ITANES2018_r
 ITANES2018 = ITANES2018_ready
 
 
+
 ###RESTRUCTURING AND CLEANING OF ITANES2013##
 ##-----------------------------------------##
 
@@ -45,10 +46,280 @@ ITANES2013 = read.spss("C:/Users/Merio/Desktop/Uni/Multivariate/FINAL EXAM/Paper
 
 
 
+#Gender Dummy, Region and Union
 
-ITANES2013 %>% 
+ITANES2013 = ITANES2013 %>% 
   mutate(
-    fct_collapse(
+    
+    gender_dummy = as.factor(
+      ifelse(sex == 'Donna', 1,0) #1 females, 0 males
+    ),
+    residence = fct_collapse(
+      regioni,
+      North = c('Lombardia', 'Piemonte', 'Liguria','Valle d\'Aosta', 'Trentino Alto Adige',
+                'Veneto', 'Friuli Venezia Giulia', 'Emilia Romagna'),
+      Center = c('Toscana', 'Umbria', 'Marche', 'Lazio', 'Abruzzo', 'Molise'),
+      South = c('Campania', 'Basilicata', 'Puglia', 'Calabria', 'Sicilia', 'Sardegna') #islands included,
+    ),
+    
+    Union = ifelse(ITANES2013$d78_7 == 'Sì', 1, 
+                   ifelse(ITANES2013$d78_7 == 'No', 0, NA)))
+    
+
+table(ITANES2013$Union )
+table(ITANES2013$residence)
+table(ITANES2013$gender_dummy)
+
+
+##POLITICAL IDEOLOGY##
+######################
+
+
+#As numeric
+
+ITANES2013 = ITANES2013 %>% 
+  mutate(Ideology_numeric = as.numeric(as.character( case_when(
+    d39 == 'Sinistra' ~ "1",
+    d39 == 'Destra' ~ "10",
+    d39 %in% as.character(1:10) ~ d39,
+    TRUE ~ NA_character_
+    )
+    )))
+
+table(ITANES2013$Ideology_numeric)
+
+
+#As categorical
+
+categories = c('Left', 'Centre-Left', 'Centre', 'Centre-Right', 'Right')
+
+ITANES2013 = ITANES2013 %>% 
+  mutate(
+    ideology_category = cut(Ideology_numeric, breaks = c(1, 3, 5, 7, 9, 11), right = F, labels = categories) 
+  )
+
+table(ITANES2013$ideology_category, ITANES2013$Ideology_numeric)
+
+
+#Dummies
+
+ITANES2013 = ITANES2013 %>% 
+  mutate(
+    Left = ifelse(ITANES2013$ideology_category == 'Left', 1, 0),
+    Right = ifelse(ITANES2013$ideology_category == 'Right', 1, 0),
+    Centre_Left = ifelse(ITANES2013$ideology_category == 'Centre-Left', 1, 0),
+    Centre_Right = ifelse(ITANES2013$ideology_category == 'Centre-Right', 1, 0),
+    None = ifelse(is.na(ITANES2013$ideology_category) == TRUE, 1, 0)
+  )
+
+
+
+##EUROPEAN UNION##
+##################
+
+
+ITANES2013$Trust_EU = ITANES2013$d15_5
+
+levels(ITANES2013$Trust_EU) [5:6] = NA
+
+table(ITANES2013$Trust_EU)
+
+
+ITANES2013$Trust_EU_numeric = as.numeric(as.factor(ITANES2013$Trust_EU))
+
+####IMMIGRATION#####
+###################
+
+#The variable in question is different from the original.
+
+
+ITANES2013$immigration = ITANES2013$d44_8
+
+levels(ITANES2013$immigration) [5:6] = NA
+
+ITANES2013$immigration = factor(ITANES2013$immigration,
+                             levels = c( "Per niente d'accordo", "Poco d'accordo",
+                                         "Abbastanza d'accordo", "Molto d'accordo"))
+
+ITANES2013$immigration_numeric = as.numeric(as.factor(ITANES2013$immigration))
+
+
+##Populist attitudes##
+#####################
+
+
+
+ITANES2013$Populism = ITANES2013$d22_5
+
+ITANES2013 <- ITANES2013 %>%
+  mutate(
+    Populism = case_when(
+      Populism %in% c("Non sa", "Non risponde") ~ NA_character_,
+      TRUE ~ as.character(Populism)
+    ),
+    Populism = factor(Populism),
+    Populism_numeric = as.numeric(Populism)
+  )
+
+
+###Trust in Political Parties###
+################################
+
+ITANES2013  = ITANES2013 %>%
+  mutate(
+    trust_parties = case_when(
+      
+      d15_2 %in% c('Non sa', 'Non risponde')~ NA_character_,
+      TRUE~ as.character(d15_2) 
+    ),
+    
+    trust_parties = as.factor(trust_parties),
+    trust_parties_numeric = as.numeric(trust_parties)
+    
+  )
+
+
+  
+##Economic hardship##
+#####################
+
+ITANES2013 = ITANES2013 %>% 
+  mutate(
+    Economic_hardship_dummy = case_when(
+      
+      
+      lav3 %in% c('Sì, di frequente', 'Sì, qualche volta')~ 1,
+      lav3 == 'No' ~ 0,
+      TRUE ~ NA_integer_
       
     )
+    
   )
+
+table(ITANES2013$Economic_hardship_dummy, useNA = 'always')
+
+
+##################################
+###### Turnout and Vote Choice####
+
+
+
+ITANES2013 = ITANES2013 %>% 
+  mutate(
+    turnout = case_when(
+      d86 == 'Sì, sono andato a votare'~ 1,
+      d86 == 'No, non sono andato a votare'~ 0,
+      TRUE  ~ NA_integer_
+    )
+    
+  )
+
+
+table(ITANES2013$turnout)
+
+
+##Political Parties:
+
+#taking only the chamber of deputees since the law didn't permit people younger than 24 to vote for the Senate.
+
+ITANES2013$voto = ITANES2013$d90
+
+ITANES2013$voto = fct_collapse(
+  ITANES2013$voto,
+  
+  Pd = 'Partito Democratico',
+  Pdl = "Il Popolo Della Liberta'",
+  M5s = 'Movimento 5 Stelle Beppegrillo.It',
+  Fdi = "Fratelli D'italia",
+  Lega = 'Lega Nord'
+  
+)
+
+#Mainstream vs Challengers.
+
+
+#Due to the low numbers of dx- challenger parties, I'm going to incorporate all the alternative right wing parties into "Challengers"
+
+
+ITANES2013$type_voto = fct_collapse(
+  ITANES2013$voto,
+  
+  "Mainstream Left" = c('Pd', 'Scelta Civica Con Monti Per L\'italia', 'Centro Democratico', 'Unione Di Centro'),
+  "Mainstream Right" = c('Pdl', 'Lega'),
+  'Challenger Left' = 'M5s',
+  'Challenger Right' = c('Fdi', 'La Destra', 'Fiamma Tricolore', 'Forza Nuova', 'Casapound Italia'),
+  'Others' = c('altro', "Lista Amnistia Giustizia Liberta'", 'Partito Comunista Dei Lavoratori', 'Mir - Moderati In Rivoluzione',
+               'Il Megafono Di Crocetta', 'Svp', 'Partito Pensionati', 'Grande Sud - Mpa', "Futuro E Liberta'", 'Fare Per Fermare Il Declino',
+               'Rivoluzione Civile'),
+  'No voto' = c('Scheda Bianca/Nulla/Non Ha Votato', 'Non Indica, Non Vuole Risponde', '-1')
+  
+)
+
+#The majority of the respondents didn't vote, this is too big of a proportion to leave out.
+
+#we can set 'others' as NA
+levels(ITANES2013$type_voto)[2] = NA
+
+sum(is.na(ITANES2013$type_voto)) / sum(!is.na(ITANES2013$type_voto)) *100 #leaving out just 3.7 percent of the total sample.
+
+
+
+table(ITANES2013$turnout, ITANES2013$type_voto)
+
+
+#Set the order:
+
+ITANES2013$type_voto = factor(
+  ITANES2013$type_voto, levels= c(
+    'Mainstream Left', 'Mainstream Right', 'Challenger Left', 'Challenger Right', 'No voto'
+  ))
+
+
+#Challenger / No vote vs Mainstream
+
+ITANES2013$voto_challenger = ITANES2013$type_voto
+
+
+levels(ITANES2013$voto_challenger)[1:2] = 'Mainstream'
+
+ITANES2013$voto_challenger = relevel(ITANES2013$voto_challenger, ref = 'Mainstream')
+
+
+
+#Mainstream/No vote  vs Challenger
+
+ITANES2013$voto_Mainstream = ITANES2013$type_voto
+
+
+levels(ITANES2013$voto_Mainstream)[3:4] = 'Challenger'
+
+ITANES2013$voto_Mainstream = relevel(ITANES2013$voto_Mainstream, ref = 'Challenger')
+
+
+#Mainstream / Challenger vs No vote
+
+ITANES2013$voto_novote = ITANES2013$type_voto
+
+
+ITANES2013$voto_novote = relevel(ITANES2013$voto_novote, ref = 'No voto')
+
+
+
+##INCUMBENT##
+############
+
+#*Since in the Itanes2013 there is not a variable similar to the one used in Itanes2018,
+#*As 'Incumbent' can be interpreted as the fear / uncertainty caused by the time of economic crisis.
+
+ITANES2013$incumbent = ITANES2013$d6
+
+levels(ITANES2013$incumbent)[6:7] = NA
+
+ITANES2013$incumbent_numeric = as.numeric(ITANES2013$incumbent)
+
+
+
+#####EMPLOYMENT AND UNEMPLOYMENT#####
+#####################################
+
+
