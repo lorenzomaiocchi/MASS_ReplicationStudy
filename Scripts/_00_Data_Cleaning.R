ad9a15c7b338c3b5f9fcf5cbad6837a4f8f53861
lorenzomaiocchi <lorenzo.maiocchi06@gmail.com>
2025-07-16 22:44:02 +0200

    added datasets, added plots scripts

--- Scripts/_00_Data_Cleaning.R
+++ Scripts/_00_Data_Cleaning.R
@@ -5,7 +5,7 @@
 
 want = c("rio", "haven", "janitor", "stargazer", "foreign", "magrittr",
          "nnet", "MASS", "MNLpred", "ggplot2", "ggplot2", "extrafont", "scales", "effects", "ggpubr",
-         "jtools", "simEd", 'tidyverse', 'forcats')
+         "jtools", "simEd", 'tidyverse', 'forcats', 'margins', 'marginaleffects')
 
 have = want %in% rownames(installed.packages())
 
@@ -21,18 +21,6 @@ rm(have, want, junk)
 set.seed(260423)
 ###########################
 
-
-
-## LOAD THE DATA ##
-
-load("C:/Users/Merio/Desktop/Uni/Multivariate/FINAL EXAM/Paper-data/ITANES2018_ready.RData")
-
-#For ITANES 2018, I'm using the pre-structured dataset made available by the author. 
-
-ITANES2018 = ITANES2018_ready
-
-
-
 ###RESTRUCTURING AND CLEANING OF ITANES2013##
 ##-----------------------------------------##
 
@@ -41,8 +29,7 @@ ITANES2018 = ITANES2018_ready
 #*re-coding them in the same way. 
 
 
-ITANES2013 = read.spss("C:/Users/Merio/Desktop/Uni/Multivariate/FINAL EXAM/Paper-data/ITA2013_(itvers2013_11_29).sav",
-                       to.data.frame = T)
+ITANES2013 = read.spss("Datasets/ITA2013_(itvers2013_11_29).sav",to.data.frame = T)
 
 
 
